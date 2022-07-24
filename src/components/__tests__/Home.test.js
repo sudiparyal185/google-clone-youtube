81a6d97d7c99e5782470a858bb68abcf5e58ab98
Sudip Aryal <sudip.aryal@nextiva.com>
2022-07-23 21:31:16 -0500

    added tests for each link elements

--- src/components/__tests__/Home.test.js
+++ src/components/__tests__/Home.test.js
@@ -4,19 +4,45 @@ import { BrowserRouter } from 'react-router-dom';
 import Home from '../Home';
 
 describe('Home component', () => {
-  it('should render with correct contents', () => {
-    render(<Home />, { wrapper: BrowserRouter });
-    const aboutLinkTag = screen.getByText(/about/i);
-    const aboutStoreTag = screen.getByText(/store/i);
-    const aboutGmailTag = screen.getByText(/gmail/i);
-    const aboutImagesTag = screen.getByText(/images/i);
-    const appIcon = screen.getByTestId('app-icon');
-    const avatarIcon = screen.getByTestId('header-avatar');
-    expect(aboutLinkTag).toBeInTheDocument();
-    expect(aboutStoreTag).toBeInTheDocument();
-    expect(aboutGmailTag).toBeInTheDocument();
-    expect(aboutImagesTag).toBeInTheDocument();
-    expect(appIcon).toBeInTheDocument();
-    expect(avatarIcon).toBeInTheDocument();
-  });
+    it('should render with correct contents', () => {
+      render(<Home />, { wrapper: BrowserRouter });
+
+      const aboutLinkTag = screen.getByText(/about/i);
+      const aboutStoreTag = screen.getByText(/store/i);
+      const aboutGmailTag = screen.getByText(/gmail/i);
+      const aboutImagesTag = screen.getByText(/images/i);
+      const appIcon = screen.getByTestId('app-icon');
+      const avatarIcon = screen.getByTestId('header-avatar');
+
+      expect(aboutLinkTag).toBeInTheDocument();
+      expect(aboutStoreTag).toBeInTheDocument();
+      expect(aboutGmailTag).toBeInTheDocument();
+      expect(aboutImagesTag).toBeInTheDocument();
+      expect(appIcon).toBeInTheDocument();
+      expect(avatarIcon).toBeInTheDocument();
+      expect(window.location.pathname).toBe('/');
+
+      userEvent.click(aboutLinkTag);
+      expect(window.location.pathname).toBe('/about');
+    });
+    it('should change the routes correclty when link are clicked', () => {
+      render(<Home />, { wrapper: BrowserRouter });
+
+      const aboutLinkTag = screen.getByText(/about/i);
+      const aboutStoreTag = screen.getByText(/store/i);
+      const aboutGmailTag = screen.getByText(/gmail/i);
+      const aboutImagesTag = screen.getByText(/images/i);
+
+      userEvent.click(aboutLinkTag);
+      expect(window.location.pathname).toBe('/about');
+
+      userEvent.click(aboutStoreTag);
+      expect(window.location.pathname).toBe('/store');
+
+      userEvent.click(aboutGmailTag);
+      expect(window.location.pathname).toBe('/gmail');
+
+      userEvent.click(aboutImagesTag);
+      expect(window.location.pathname).toBe('/Images');
+    });
 });
