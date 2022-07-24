b66eb52b855f885c42514c27b385273b55b3ec8f
Sudip Aryal <sudip.aryal@nextiva.com>
2022-07-24 08:58:36 -0500

    finished test cases for search input component

--- src/components/__tests__/SearchInput.test.js
+++ src/components/__tests__/SearchInput.test.js
@@ -12,7 +12,11 @@ describe('SearchInput', () => {
       </BrowserRouter>,
     );
   });
-  it('should render the componets correctly', () => {
+
+  afterEach(() => {
+    cleanup();
+  });
+  it('should render the components correctly', () => {
     const searchIcon = screen.getByTestId('search-icon');
     const micIcon = screen.getByTestId('mic-icon');
     const searchField = screen.getByTestId('search-input');
@@ -23,11 +27,43 @@ describe('SearchInput', () => {
       name: /i'm feeling lucky/i,
     });
 
+    expect(searchField).toHaveValue('');
     expect(searchIcon).toBeInTheDocument();
     expect(micIcon).toBeInTheDocument();
     expect(searchField).toBeInTheDocument();
     expect(googleSearchButton).toBeInTheDocument();
     expect(feelingLuckyButton).toBeInTheDocument();
   });
-  it.todo('should update the url when something is typed and form is updated');
+  it('should update the url when something is typed and form is updated', () => {
+    const searchField = screen.getByTestId('search-input');
+    const googleSearchButton = screen.getByRole('button', {
+      name: /google search/i,
+    });
+
+    userEvent.type(searchField, 'tesla');
+    expect(searchField).toHaveValue('tesla');
+
+    userEvent.click(googleSearchButton);
+    expect(window.location.search).toBe('?tesla');
+  });
+});
+
+describe('SearchInput component', () => {
+  it('should not show buttons if the showButtons flag is set to false', () => {
+    render(
+      <BrowserRouter>
+        <SearchInput showButtons={false} />
+      </BrowserRouter>,
+    );
+
+    const googleSearchButton = screen.queryByRole('button', {
+      name: /google search/i,
+    });
+    const feelingLuckyButton = screen.queryByRole('button', {
+      name: /i'm feeling lucky/i,
+    });
+
+    expect(googleSearchButton).not.toBeInTheDocument();
+    expect(feelingLuckyButton).not.toBeInTheDocument();
+  });
 });
